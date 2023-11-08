2b35d5da7ffe68134c2dabcca25b07772a9d3d75
Kyle Butts <buttskyle96@gmail.com>
2023-11-08 18:03:44 -0500

    Add solutions

---
+++ Labs/Lab-1/Solutions/vam.R
@@ -0,0 +1,238 @@
+# ------------------------------------------------------------------------------
+# Kyle Butts & Chris Walters
+# 10/2023
+# This program simulates data from a school value-added model
+# and estimates the model parameters using empirical Bayes methods
+
+library(fixest)
+library(data.table)
+library(broom)
+library(glue)
+library(here)
+library(tidyverse)
+set.seed(1028)
+
+# Change:
+setwd(here::here("Labs/Lab-1"))
+
+n_students = 2500
+n_schools = 50
+sigma_y = 1
+sigma_theta = 0.2
+df = read_csv("vam_example_data.csv")
+
+# Estimate value-added models --------------------------------------------------
+
+### No controls ----------------------------------------------------------------
+
+est_unc = feols(
+  Y ~ 0 + i(school_id), df, vcov = "hc1"
+)
+
+# Extract value-added estimates
+va_unc = as.data.table(broom::tidy(est_unc))
+va_unc = va_unc[grepl("school_id::", va_unc$term), ]
+va_unc$school_id = as.numeric(gsub("school_id::", "", va_unc$term))
+
+va_unc = va_unc[, .(school_id, thetahat_unc = estimate, se_unc = std.error)]
+
+### With control for X ---------------------------------------------------------
+
+est_con = feols(
+  Y ~ 0 + i(school_id) + X, df, vcov = "hc1"
+)
+
+va_con = as.data.table(broom::tidy(est_con))
+va_con = va_con[grepl("school_id::", va_con$term), ]
+va_con$school_id = as.numeric(gsub("school_id::", "", va_con$term))
+
+va_con = va_con[, .(school_id, thetahat_con = estimate, se_con = std.error)]
+
+## Collapse to school-level ----------------------------------------------------
+
+# Get number of students and true value added for each school
+school_va = df[, .(N = .N, theta = theta[1]), by = "school_id"]
+
+# Merge in estimates
+school_va = merge(school_va, va_unc, by = "school_id")
+school_va = merge(school_va, va_con, by = "school_id")
+
+## Estimate hyperparameters for each model -------------------------------------
+
+#' @param thetahat Vector of school-level estimates
+#' @param se Vector of standard errors of school-level estimates
+#' @return List with estimated hyperparameters `muhat` and `sigmahat`
+est_hyperparameters = function(thetahat, se) {
+  muhat = mean(thetahat)
+  c = (thetahat - muhat)^2 - se^2
+
+  c_mean = mean(c)
+  # If mean(c) < 0, then the we should set c = 0
+  c_mean = max(c_mean, 0)
+
+  sigmahat = sqrt(c_mean)
+
+  return(list(muhat = muhat, sigmahat = sigmahat))
+}
+
+hp_unc = est_hyperparameters(school_va$thetahat_unc, school_va$se_unc)
+hp_con = est_hyperparameters(school_va$thetahat_con, school_va$se_con)
+
+# Display results
+glue("Estimated hyperparameters for model `unc`: mean = {round(hp_unc$muhat, 2)}, SD = {round(hp_unc$sigmahat, 3)}")
+glue("Estimated hyperparameters for model `con`: mean = {round(hp_con$muhat, 2)}, SD = {round(hp_con$sigmahat, 3)}")
+
+## Compute linear shrinkage estimates ------------------------------------------
+
+#' @param thetahat Vector of school-level estimates
+#' @param se Vector of standard errors of school-level estimates
+#' @param muhat Estimated mean of school-level estimates (hyperparameter)
+#' @param sigmahat Estimated SD of school-level estimates (hyperparameter)
+#' @return Vector of linear shrinkage estimates
+linear_shrinkage <- function(thetahat, se, muhat, sigmahat) {
+  # Shrinkage factor 
+  lambda = sigmahat^2 / (sigmahat^2 + se^2)
+
+  # Linear shrinkage formula
+  thetastar = lambda * thetahat + (1 - lambda) * muhat
+}
+
+school_va$thetastar_unc = linear_shrinkage(
+  school_va$thetahat_unc, school_va$se_unc, 
+  hp_unc$muhat, hp_unc$sigmahat
+)
+
+school_va$thetastar_con = linear_shrinkage(
+  school_va$thetahat_con, school_va$se_con, 
+  hp_con$muhat, hp_con$sigmahat
+)
+
+## Assess performance of estimators --------------------------------------------
+
+# Compare dispersion of unbiased estimates, estimated prior, shrunk posteriors, and true parameters
+SD_true = sd(school_va$theta)
+
+SD_unc_1 = sd(school_va$thetahat_unc)
+SD_unc_2 = sd(school_va$thetastar_unc)
+
+glue("
+  Model `unc`: 
+    SD estimates = {round(SD_unc_1, 3)}, 
+    SD prior = {hp_unc$sigmahat}, 
+    SD posteriors = {SD_unc_2}, 
+    SD truth = {SD_true}
+")
+
+SD_con_1 = sd(school_va$thetahat_con)
+SD_con_2 = sd(school_va$thetastar_con)
+glue("
+  Model `con`: 
+    SD estimates = {round(SD_con_1, 3)}, 
+    SD prior = {hp_con$sigmahat}, 
+    SD posteriors = {SD_con_2}, 
+    SD truth = {SD_true}
+")
+
+# Compare MSE of each estimate
+calc_mse = function(est, true) mean((est - true)^2)
+
+MSE_thetahat_unc = calc_mse(school_va$thetahat_unc, school_va$theta)
+MSE_thetastar_unc = calc_mse(school_va$thetastar_unc, school_va$theta)
+MSE_thetahat_con = calc_mse(school_va$thetahat_con, school_va$theta)
+MSE_thetastar_con = calc_mse(school_va$thetastar_con, school_va$theta)
+
+glue("
+  MSE thetahat_unc:  {round(MSE_thetahat_unc, 3)}
+  MSE thetastar_unc: {round(MSE_thetastar_unc, 3)}
+  MSE thetahat_con:  {round(MSE_thetahat_con, 3)}
+  MSE thetastar_con: {round(MSE_thetastar_con, 3)}
+")
+
+
+## Two-way plot of truth and estimate ------------------------------------------
+
+# For controlled model, graph true VA against unbiased estimates and linear shrinkage estimates, along w/45-deg line
+
+# Make plot square
+extent = max(abs(range(school_va$theta, school_va$thetahat_con, school_va$thetastar_con)))
+limits = c(-extent, extent) * 1.1
+
+ggplot(school_va) + 
+  geom_point(
+    aes(x = thetahat_con, y = theta, color = "thetahat"),
+    size = 2
+  ) +
+  geom_point(
+    aes(x = thetastar_con, y = theta, color = "thetastar"),
+    size = 2
+  ) +
+  geom_abline(
+    slope = 1, intercept = 0, 
+    color = "grey", linetype = "dashed"
+  ) +
+  scale_color_manual(
+    values = c("thetahat" = "navy", "thetastar" = "maroon"), 
+    labels = c("thetahat" = "Unbiased Estimates", "thetastar" = "Linear Shrinkage Estimates")
+  ) +
+  # Make scale of x and y axes the same
+  scale_x_continuous(
+    limits = limits
+  ) + 
+  scale_y_continuous(
+    limits = limits
+  ) +
+  labs(
+    x = "Estimated value-added",
+    y = "True value-added",
+    color = NULL
+  ) +
+  theme_bw(base_size = 20) + 
+  theme(
+    legend.position = "bottom"
+  )
+
+## Histogram of estimates ------------------------------------------------------
+
+width = 0.06
+binwidth = 0.06 / 1.5
+prior_dist_func = function(x) {
+  n_schools * width * dnorm(x, hp_con$muhat, hp_con$sigmahat)
+}
+hist_limits = c(-4 * sigma_theta, 4 * sigma_theta)
+
+ggplot(school_va) + 
+  geom_histogram(
+    aes(x = thetahat_con, color = "thetahat", fill = "thetahat"),
+    binwidth = binwidth
+  ) +
+  geom_histogram(
+    aes(x = thetastar_con, color = "thetastar", fill = "thetastar"),
+    binwidth = binwidth
+  ) +
+  # Plot prior distribution
+  stat_function(
+    fun = prior_dist_func, 
+    color = "grey20", linewidth = 1
+  ) +
+  scale_color_manual(
+    values = c("thetahat" = "navy", "thetastar" = "maroon"), 
+    labels = c("thetahat" = "Unbiased Estimates", "thetastar" = "Linear Shrinkage Estimates")
+  ) +
+  scale_fill_manual(
+    values = c("thetahat" = "#ffffff00", "thetastar" = "#b0306055"), 
+    labels = c("thetahat" = "Unbiased Estimates", "thetastar" = "Linear Shrinkage Estimates")
+  ) +
+  scale_x_continuous(limits = hist_limits) +
+  labs(
+    x = "Estimated value-added",
+    y = "True value-added",
+    title = "Plot of value-added estimates and prior distribution",
+    color = NULL, fill = NULL
+  ) +
+  theme_bw(base_size = 20) + 
+  theme(
+    legend.position = "bottom"
+  )
+
+
+
---
+++ Labs/Lab-1/Solutions/vam.do
@@ -0,0 +1,179 @@
+
+
+*******************************************
+**** Chris Walters
+**** 10/2023
+**** This program estimates the model parameters using empirical Bayes methods
+********************************************
+
+	********************
+	*** Basic setup ****
+	********************
+	
+		clear
+		cap set more off
+		cap set trace off
+		cap log close
+		set seed 1028
+		cd "Labs/Lab-1/"
+	
+		****Load data
+		insheet using "vam_example_data.csv", names comma clear
+		ren y Y
+		ren x X
+		ren d D
+		ren theta_d theta_D
+
+    *Number of schools
+    local J=50
+	
+		****Generate school attendance dummies and sample sizes
+			sum D, detail
+			local J=r(max)
+			foreach j of numlist 1/`J' {
+				gen D_`j'=(D==`j')
+				count if D==`j'
+				gen N`j'=r(N)
+			}
+	
+		******Value-added models
+			
+			***Fit VAM regressions
+			
+				**No controls
+				
+					*Run regression
+					reg Y D_*, r nocons
+					
+					*Collect estimates and standard errors
+					foreach j of numlist 1/`J' {
+						gen thetahat_unc`j'=_b[D_`j']
+						gen s_unc`j'=_se[D_`j']
+					}
+				
+				**With control for X
+
+					*Run regression
+					reg Y D_* X, r nocons
+					
+					*Collect estimates and standard errors
+					foreach j of numlist 1/`J' {
+						gen thetahat_con`j'=_b[D_`j']
+						gen s_con`j'=_se[D_`j']
+					}
+					
+
+			***Collapse to school-level data set with true and estimated VA
+			foreach j of numlist 1/`J' {
+				sum theta_D if D==`j'
+				gen theta`j'=r(mean)
+				
+			}
+			drop theta_D
+			keep theta* s_* N*
+			duplicates drop
+			gen i=1
+			reshape long theta thetahat_unc s_unc thetahat_con s_con N, i(i) j(j)
+			drop i
+
+		
+		******Estimate hyperparameters for each model
+		foreach m in unc con {
+			
+			*Mean
+			sum thetahat_`m'
+			local muhat_`m'=r(mean)
+			
+			*Standard dev
+			gen c=(thetahat_`m'-(`muhat_`m''))^2 - s_`m'^2
+			sum c
+			local sigmahat_`m'=sqrt(r(mean))
+			drop c
+				
+			*Display results
+			disp "Estimated hyperparameters for model `m': mean = `muhat_`m'', SD = `sigmahat_`m''"
+			
+		}
+			
+		****Compute linear shrinkage estimates for the controlled model
+		foreach m in unc con {
+		
+			*Shrinkage factor
+			gen lambda_`m'=((`sigmahat_`m'')^2)/(((`sigmahat_`m'')^2)+(s_`m'^2))
+			
+			*Linear shrinkage formula
+			gen thetastar_`m'=lambda_`m'*thetahat_`m'+(1-lambda_`m')*(`muhat_`m'')
+		
+		}
+	
+	
+	***************************************
+	*** Assess performance of estimators **
+	***************************************
+	
+		*****Compare dispersion of unbiased estimates, estimated prior, shrunk posteriors, and true parameters
+		foreach m in unc con {
+		
+			*Unbiased estimates
+			sum thetahat_`m'
+			local SD_1=r(sd)
+			
+			*Shrunk posteriors
+			sum thetastar_`m'
+			local SD_2=r(sd)
+			
+			*True parameters
+			sum theta
+			local SD_3=r(sd)
+			
+			*Report comparison
+			disp "Model `m': SD estimates=`SD_1', SD prior =`sigmahat_`m'', SD posteriors=`SD_2', SD truth = `SD_3'"
+
+		
+		}
+		
+		****Compare MSE of each estimator
+		foreach e in thetahat_unc thetastar_unc thetahat_con thetastar_con {
+		
+			gen MSE_`e'=(`e'-theta)^2
+		
+		}
+		gen MSE_diff = MSE_thetahat_con - MSE_thetastar_con
+		sum MSE*, detail
+		
+		
+		****For controlled model, graph true VA against unbiased estimates and linear shrinkage estimates, along w/45-deg line
+		graph twoway scatter theta thetahat_con, mcolor(navy) ///
+			|| scatter theta thetastar_con, mcolor(maroon) ///
+			|| function y=x, range(thetahat_con) lcolor(black) lpattern(dash) ///
+			xtitle("Estimated value-added") ytitle("True value-added") ///
+			legend(lab(1 "Unbiased estimates") lab(2 "Linear shrinkage estimates")) ///
+			scheme(s1color)
+			
+		****Histograms of unbiased estimates, prior, posteriors
+		local N=_N
+		local width=0.06
+		local bwidth=`width'/1.5
+    
+    * For the figure, the true sd of theta
+    local sigma_theta = 0.2
+		local min = -4*`sigma_theta'
+		local max = 4*`sigma_theta'
+    
+		graph twoway hist thetahat_con, freq width(`width') color(navy) fintensity(0) ///
+			|| hist thetastar_con, freq width(`width') barwidth(`bwidth') color(maroon) ///
+			|| function y = (`N'*`width')*normalden(x,`muhat_con',`sigmahat_con'), range(`min' `max') lcolor(black) ///
+			xtitle("Value-added") ytitle("Schools (frequency)") ///
+			legend(lab(1 "Unbiased estimates") lab(2 "Linear shrinkage estimates") lab(3 "Prior distribution") order(1 3 2)) ///
+			scheme(s1color)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
